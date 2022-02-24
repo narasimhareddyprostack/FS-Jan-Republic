4cdaf2fc7c564686ba0f7a6f5426caa07cc11f57
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-02-24 10:01:40 +0530

    employeedata

---
+++ JSPart2/ObjCRUD/User.js
@@ -0,0 +1,12 @@
+let User = {
+    id: 101,
+    name: "Rahul Gandhi",
+    get_Detail: function () {
+        console.log("GM")
+    }
+}
+console.log(User.id);
+console.log(User.name);
+console.log(User.loc);
+User.get_Detail()
+User.getName()
---
+++ JSPart2/ObjCRUD/emp.js
@@ -0,0 +1,8 @@
+let emp = { id: 101, name: "Rahul Gandhi", sal: 45000 }
+console.log(emp)
+console.log(emp.id)
+console.log(emp.name)
+console.log(emp.sal)
+emp.sal = 55999;
+console.log("*** After Getting Hike****");
+console.log(emp)
