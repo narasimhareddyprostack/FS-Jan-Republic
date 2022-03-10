b7c1fdaba44943527a87d2a83cbb8a0a982a113a
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-10 10:06:28 +0530

    okie

---
+++ ObjDestruct/one.js
@@ -0,0 +1,8 @@
+let employee = { id: 101, name: "Rahul", email: "Rahul@gmail.com", salary: 340000 }
+
+let { name, salary } = employee
+console.log(name)
+console.log(salary)
+/*
+console.log(employee.salary)
+console.log(employee.name) */
---
+++ ObjDestruct/two.js
@@ -0,0 +1,8 @@
+let obj1 = { id: 101 }
+var { id } = obj1
+
+console.log(id)
+
+let obj2 = { id: 102 }
+var { id } = obj2
+console.log(id)
