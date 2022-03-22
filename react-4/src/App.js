ffa0de22ed9ee83152416ab62be6b50f4704b8df
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-22 10:28:49 +0530

    okie

---
+++ react-4/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+
+    return <div>
+        <h1>Hello,GM</h1>
+        <h2>Hello,GM</h2>
+        <h3>Hello,GM</h3>
+        <h4>Hello,GM</h4>
+        <h5>Hello,GM</h5>
+        <p>Multidisciplinary Web Template Built with Your Favourite Technology - HTML Bootstrap, Tailwind and React NextJS.</p>
+    </div>
+}
+export default App
